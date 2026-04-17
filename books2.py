f9c7efce9d05acf1103b2869491f62b3f095fb31
Azizbek Rakhmatullayev <rakhmatullayevbrb@gmail.com>
2026-04-17 14:01:58 +0500

    I added new requests

--- books2.py
+++ books2.py
@@ -1,18 +1,20 @@
 from typing import Optional
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Path,Query, Body
 from pydantic import BaseModel, Field
 app=FastAPI()
 
 class Book:
     id: int
+    published_date: int
     title: str
     author: str
     description: str
     rating: int
 
-    def __init__(self,id,title,author,description,rating):
+    def __init__(self,id,published_date, title,author,description,rating):
         self.id = id
+        self.published_date=published_date
         self.title = title
         self.author = author
         self.description=description
@@ -20,6 +22,7 @@ class Book:
 
 class BookRequest(BaseModel):
     id: Optional[int]=Field(description="ID is not needed on create", default=None)
+    published_date: int=Field(gt=1999, lt=2027)
     title:str=Field(min_length=3)
     author:str=Field(min_length=1)
     description:str=Field(min_length=1,max_length=100)
@@ -28,8 +31,9 @@ class BookRequest(BaseModel):
     model_config={
         "json_schema_extra":{
             "example":{
+                "published_date": "2026",
                 "title": "A new book",
-                "author": "Codingwith",
+                "author": "Coding with",
                 "description": "A new description of a book",
                 "rating": 5,
             }
@@ -38,19 +42,38 @@ class BookRequest(BaseModel):
     }
 
 BOOKS=[
-    Book(1,"Computer science", "codingwith","A very nice book", 3),
-    Book(2,"Pyhton","Angela Yu","really good course",5),
-    Book(3,"Elcetronic ", "John","A nice course", 4),
-    Book(4,"math","Yu"," good course",1),
-    Book(5,"Computer engineering", "the real one","A very nice course", 3),
-    Book(6,"pythics","Master","not bad",2),
+    Book(1,2025,"Computer science", "codingwith","A very nice book", 3),
+    Book(2,2012,"Pyhton","Angela Yu","really good course",5),
+    Book(3,2023,"Elcetronic ", "John","A nice course", 4),
+    Book(4,2005,"math","Yu"," good course",3),
+    Book(5,2026,"Computer engineering", "the real one","A very nice course", 3),
+    Book(6,2022,"pythics","Master","not bad",2),
 ]
 
+BOOK_ID=[]
 
 @app.get("/books")
 async def read_all_books():
     return BOOKS
 
+
+@app.get("/books/publish/")
+async def read_date_of_book(book_date: int=Query(gt=1989, lt=2027)):
+    books_to_return=[]
+    for i in range(len(BOOKS)):
+        if BOOKS[i].published_date==book_date:
+            books_to_return.append(BOOKS[i])
+            return books_to_return
+    return None
+
+# async def date(publish_date: int):
+#     to_return=[]
+#     for book in BOOKS:
+#         if book.published_date==publish_date:
+#             to_return.append(book)
+#             return to_return
+#     return None
+#
 
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
@@ -58,7 +81,57 @@ async def create_book(book_request: BookRequest):
     print(type(new_book))
     BOOKS.append(add_id(new_book))
 
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id==book.id:
+            BOOKS[i]=book
+    return BOOKS[i]
+
+@app.delete("/books/delete_book/{book_id}")
+async def delete_book(book_id: int=Path(gt=0)):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id==book_id:
+            BOOKS.pop(i)
+            break
+
+
+
+@app.get("/books/{book_id}")
+async def read_book_by_id(book_id: int=Path(gt=0)):
+    for book in BOOKS:
+        if book.id==book_id:
+            # BOOK_ID.append(book)
+            return book
+    return None
+
+
+# @app.get("/books/{published_date}")
+# async def get_book_by_date(published_date: int):
+#     books_by_date=[]
+#     for book in BOOKS:
+#         if book.published_date==published_date:
+#             books_by_date.append(book)
+#             return books_by_date
+#     return None
 #
+
+
+
+
+
+
+
+
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int=Query(gt=-1, lt=6)):
+    books_to_return=[]
+    for book in BOOKS:
+        if book.rating==book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
 # def find_book_id(book: Book):
 #     if len(BOOKS)>0:
 #         book.id=BOOKS[-1].id+1
